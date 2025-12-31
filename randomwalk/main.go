c7ad49cec734abca5f5cd0b564bede4cdb8c1fd7
Aziz Rmadi <azizrmadi@gmail.com>
2025-12-31 16:04:57 +0100

    gracefully handle exits

--- randomwalk/main.go
+++ randomwalk/main.go
@@ -74,12 +74,13 @@ func main() {
 	args := os.Args
 	if len(args) > 2 {
 		fmt.Printf("Usage: %s <num-of-agents>\n", args[0])
-		os.Exit(1)
+		os.Exit(-1)
 	}
 	if len(args) == 2 {
 		customNumAgents, err := strconv.Atoi(args[1])
 		if err != nil {
 			fmt.Printf("Invalid number for agents: %s", args[1])
+			os.Exit(-1)
 		}
 		numAgents = customNumAgents
 	}
