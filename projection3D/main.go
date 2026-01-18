8b7ca5b4b612cafd9d0435f976add9341087a653
Aziz Rmadi <azizrmadi@gmail.com>
2026-01-18 12:43:55 +0100

    ebiten initialization

--- projection3D/main.go
+++ projection3D/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+var FOREGROUND_COLOR = color.RGBA{0, 128, 0, 1}
+
 type Game struct {
+	canvas *ebiten.Image
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Placeholder for 3D projection drawing logic
+	screen.DrawImage(g.canvas, nil)
 }
 
 func (g *Game) Update() error {
-	// Placeholder for updating game state
+
+	rect := ebiten.NewImage(10, 10)
+	rect.Fill(FOREGROUND_COLOR)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(100, 100)
+
+	g.canvas.DrawImage(rect, op)
+
 	return nil
 }
 
@@ -22,11 +33,19 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return 640, 480
 }
 
+func NewGame() *Game {
+	canvas := ebiten.NewImage(640, 480)
+	canvas.Fill(color.Black)
+	return &Game{
+		canvas: canvas,
+	}
+}
+
 func main() {
 
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("3D Projection Demo")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 
