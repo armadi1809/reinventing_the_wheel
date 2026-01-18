6285839d36e2e952ab526f5ccfbbca42b6f46fe9
Aziz Rmadi <azizrmadi@gmail.com>
2026-01-18 13:44:23 +0100

    basic example of projection working

--- projection3D/main.go
+++ projection3D/main.go
@@ -7,24 +7,48 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+type Point struct {
+	x, y, z float64
+}
+
+const WIDTH = 640
+const HEIGHT = 480
+
 var FOREGROUND_COLOR = color.RGBA{0, 128, 0, 1}
 
 type Game struct {
 	canvas *ebiten.Image
+	points []Point
+	dz     float64
+}
+
+func screenPosition(x, y float64) (float64, float64) {
+	return (x + 1) * 0.5 * WIDTH, (1 - (y+1)*0.5) * HEIGHT
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.canvas, nil)
 }
 
+func (g *Game) updateAndDrawPoints() {
+	for _, p := range g.points {
+		newZ := p.z + g.dz
+		newX, newY := p.x/newZ, p.y/newZ
+
+		x, y := screenPosition(newX, newY)
+		size := 10.0
+		rect := ebiten.NewImage(int(size), int(size))
+		rect.Fill(FOREGROUND_COLOR)
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(x-size/2, y-size/2)
+		g.canvas.DrawImage(rect, op)
+	}
+}
+
 func (g *Game) Update() error {
-
-	rect := ebiten.NewImage(10, 10)
-	rect.Fill(FOREGROUND_COLOR)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(100, 100)
-
-	g.canvas.DrawImage(rect, op)
+	g.canvas.Clear()
+	g.dz += 1 / float64(ebiten.TPS())
+	g.updateAndDrawPoints()
 
 	return nil
 }
@@ -38,12 +62,20 @@ func NewGame() *Game {
 	canvas.Fill(color.Black)
 	return &Game{
 		canvas: canvas,
+		points: []Point{
+			{0.5, 0.5, 0},
+			{-0.5, 0.5, 0},
+			{0.5, -0.5, 0},
+			{-0.5, -0.5, 0},
+		},
+		dz: 0,
 	}
 }
 
 func main() {
 
 	ebiten.SetWindowSize(640, 480)
+	ebiten.SetTPS(60)
 	ebiten.SetWindowTitle("3D Projection Demo")
 	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
