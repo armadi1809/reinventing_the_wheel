6d559b279c9a8692df2d5dccce5dfebdd8fd18c3
Aziz Rmadi <azizrmadi@gmail.com>
2026-05-26 11:20:10 +0200

    setup environment

--- lizp/main.c
+++ lizp/main.c
@@ -64,10 +64,73 @@ typedef struct lval
     struct lval **cell;
 } lval;
 
+typedef struct lenv
+{
+    int count;
+    char **syms;
+    lval **vals;
+} lenv;
+
 typedef lval *(*lbuiltin)(lenv *, lval *);
 
 void lval_print(lval *v);
 lval *lval_eval(lval *v);
+
+lenv *new_env(void)
+{
+    lenv *e = malloc(sizeof(lenv));
+    e->count = 0;
+    e->syms = NULL;
+    e->vals = NULL;
+    return e;
+}
+
+void lenv_del(lenv *e)
+{
+    for (int i = 0; i < e->count; i++)
+    {
+        free(e->syms[i]);
+        lval_del(e->vals[i]);
+    }
+    free(e->syms);
+    free(e->vals);
+    free(e);
+    return;
+}
+
+lval *lenv_get(lenv *e, lval *k)
+{
+    for (int i = 0; i < e->count; i++)
+    {
+        if (strcmp(e->syms[i], k->sym) == 0)
+        {
+            return lval_copy(e->vals[i]);
+        }
+    }
+
+    return lval_err("Unbound symbol");
+}
+
+void *lenv_put(lenv *e, lval *k, lval *v)
+{
+    for (int i = 0; i < e->count; i++)
+    {
+        if (strcmp(e->syms[i], k->sym) == 0)
+        {
+            lval_del(e->vals[i]);
+            e->vals[i] = lval_copy(v);
+            return;
+        }
+    }
+
+    e->count++;
+    e->vals = realloc(e->vals, sizeof(lval *) * e->count);
+    e->syms = realloc(e->syms, sizeof(char *) * e->count);
+
+    e->vals[e->count - 1] = lval_copy(v);
+    e->syms[e->count - 1] = malloc(strlen(k->sym) + 1);
+    strcpy(e->syms[e->count - 1], k->sym);
+}
 
 lval *lval_num(long x)
 {
